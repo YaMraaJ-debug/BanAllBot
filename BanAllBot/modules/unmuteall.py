365490e0098ba4c69ef2fdf7d0c5f358c6270434
Sourcery AI <>
2023-12-24 00:35:31 +0000

    'Refactored by Sourcery'

--- BanAllBot/modules/unmuteall.py
+++ BanAllBot/modules/unmuteall.py
@@ -4,19 +4,19 @@ from pyrogram.types import ChatPermissions
 
 @app.on_message(filters.command("unmuteall") & filters.user(SUDO))
 async def unmute_all(_,msg):
-    chat_id=msg.chat.id   
-    x = 0
+    chat_id=msg.chat.id
     bot=await app.get_chat_member(chat_id,BOT_ID)
-    bot_permission=bot.privileges.can_restrict_members==True 
+    bot_permission=bot.privileges.can_restrict_members==True
     if bot_permission:
         banned_users = []
+        x = 0
         async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.RESTRICTED):
             banned_users.append(m.user.id)       
             try:
                     await app.restrict_chat_member(chat_id,banned_users[x], ChatPermissions(can_send_messages=True,can_send_media_messages=True,can_send_polls=True,can_add_web_page_previews=True,can_invite_users=True))
                     await msg.reply_text(f"ᴜɴᴍᴜᴛɪɴɢ ᴀʟʟ ᴍᴇᴍʙᴇʀs {m.user.mention}")
                     x += 1
-                                        
+
             except Exception as e:
                 print(e)
     else:
